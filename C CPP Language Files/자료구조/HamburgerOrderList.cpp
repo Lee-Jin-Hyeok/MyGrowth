dc2f628030b1b02288a048d86b34ba14a3278e1b
gimhaesanai <jinjin0816@naver.com>
2019-09-30 17:54:25 +0900

    Javascript

--- C CPP Language Files/자료구조/HamburgerOrderList.cpp
+++ C CPP Language Files/자료구조/HamburgerOrderList.cpp
@@ -20,12 +20,47 @@ typedef struct _hamburgerOrderQueueList {
 typedef HamburgerOrderQueueList List;
 
 void ListInit(List*);
-void Insert(List*, Time);
+void Enqueue(List*, Time);
+Time Dequeue(List*);
 void FindAll(List*);
-Time Remove(List*);
 int isEmpty(List*);
+int Count(List*);
 
 int main() {
 
 	return 0;
+}
+
+void ListInit(List* list) {
+	list->front = NULL;
+	list->rear = NULL;
+	list->temp = NULL;
+	list->numOfData = 0;
+}
+
+void Enqueue(List* list, Time time) {
+	Node* newNode = (Node*)malloc(sizeof(Node));
+	newNode->next = NULL;
+}
+
+Time Dequeue(List* list) {
+
+	return 1;
+}
+
+void FindAll(List* list) {
+
+}
+
+int isEmpty(List* list) {
+	if ((list->numOfData) == 0) {
+		return TRUE;	// Empty
+	}
+	else {
+		return FALSE;	// non-Empty
+	}
+}
+
+int Count(List* list) {
+	return list->numOfData;
 }

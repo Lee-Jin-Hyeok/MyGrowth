3e296b245c21f9fe1c3a77cef58ff71f430f868d
gimhaesanai <jinjin0816@naver.com>
2019-10-01 09:30:32 +0900

    Tree Linked List

---
+++ C CPP Language Files/자료구조/TreeLinkedList.cpp
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int Data;
+
+typedef struct _node {
+	Data data;
+	struct _node* left;
+	struct _node* right;
+} Node;
+
+Node* MakeTreeNode(void);
+Data GetNode(Node*);
+Node* GetLeftSubTree(Node*);
+Node* GetRightSubTree(Node*);
+void MakeLeftSubTree(Node*, Node*);
+void MakeRightSubTree(Node*, Node*);
+void InorderTraverse(Node*);
+
+int main() {
+
+	return 0;
+}
+
+Node* MakeTreeNode() {
+	Node* nd = (Node*)malloc(sizeof(Node));
+	nd->left = NULL;
+	nd->right = NULL;
+	return nd;
+}
+
+Data GetNode(Node* nd) {
+	return nd->data;
+}
+
+Node* GetLeftSubTree(Node* nd) {
+	return nd->left;
+}
+
+Node* GetRightSubTree(Node* nd) {
+	return nd->right;
+}
+
+void MakeLeftSubTree(Node* nd, Node* sub) {
+	if (nd->left != NULL)
+		free(nd->left);
+
+	nd->left = sub;
+}
+
+void MakeRightSubTree(Node * nd, Node * sub) {
+	if (nd->right != NULL)
+		free(nd->right);
+
+	nd->right = sub;
+}
+
+void InorderTraverse(Node * nd) {
+	if (nd == NULL)
+		return;
+	InorderTraverse(nd->left);
+	printf("%d \n", nd->data);
+	InorderTraverse(nd->right);
+}
+
+void PreorderTraverse(Node * nd) {
+	printf("%d \n", nd->data);
+	if (nd == NULL)
+		return;
+	PreorderTraverse(nd->left);
+	PreorderTraverse(nd->right);
+}
+
+void PostorderTraverse(Node * nd) {
+	if (nd == NULL)
+		return;
+	PostorderTraverse(nd->left);
+	PostorderTraverse(nd->right);
+	printf("%d \n", nd->data);
+}

855c5f6ee1b112e10162245f39647499d4ff2936
gimhaesanai <jinjin0816@naver.com>
2019-10-10 23:31:17 +0900

    Spring study

---
+++ Spring Framework Files/SpringProject/src/main/java/com/javalec/ex/MyCalculator.java
@@ -0,0 +1,33 @@
+package com.javalec.ex;
+
+public class MyCalculator {
+	private Calculator calculator;
+	private int firstNum;
+	private int secondNum;
+	
+	public void setCalculator(Calculator calculator) {
+		this.calculator = calculator;
+	}
+	public void setFristNum(int firstNum) {
+		this.firstNum = firstNum;
+	}
+	public void setSecondNum(int secondNum) {
+		this.secondNum = secondNum;
+	}
+	
+	public void add() {
+		calculator.addition(firstNum, secondNum);
+	}
+	
+	public void sub() {
+		calculator.subtraction(firstNum, secondNum);
+	}
+	
+	public void mul() {
+		calculator.multiplication(firstNum, secondNum);
+	}
+	
+	public void div() {
+		calculator.division(firstNum, secondNum);
+	}
+}

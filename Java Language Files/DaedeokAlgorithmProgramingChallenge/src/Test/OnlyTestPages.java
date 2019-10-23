f1369515c56190c423da80037120348994802fd9
gimhaesanai <jinjin0816@naver.com>
2019-10-23 20:04:31 +0900

    HangManProject

--- Java Language Files/DaedeokAlgorithmProgramingChallenge/src/Test/OnlyTestPages.java
+++ Java Language Files/DaedeokAlgorithmProgramingChallenge/src/Test/OnlyTestPages.java
@@ -1,8 +1,21 @@
 package Test;
 
+class Person {
+	public int a;
+	int b;
+	
+	public void Init(int a, int b) {
+		this.a = a;
+		this.b = b;
+	}
+	
+	public void add(int a, int b) {
+		System.out.println(a + b );
+	}
+}
+
 public class OnlyTestPages {
 	public static void main(String[] args) {
-		int[] arr = new int[10];
-		System.out.println(arr.length);
+		
 	}
 }

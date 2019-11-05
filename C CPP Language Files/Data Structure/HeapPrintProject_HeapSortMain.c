a2ee84d6338a860631a66cc81780f149e9a70150
gimhaesanai <jinjin0816@naver.com>
2019-11-05 23:19:03 +0900

    Perfact Implemented HeapPrint

--- C CPP Language Files/Data Structure/HeapPrintProject_HeapSortMain.c
+++ C CPP Language Files/Data Structure/HeapPrintProject_HeapSortMain.c
@@ -2,7 +2,8 @@
 #include <math.h>
 #include "HeapSort.h"
 
-int DataPriorityComp(int ch1, int ch2) {
+int DataPriorityComp(int ch1, int ch2)
+{
 	//return ch2-ch1;		// Min Heap
 	return ch1 - ch2;			// Max Heap
 }
@@ -17,60 +18,60 @@ void PrintHeap(Heap* heap, char* szTitle) {
 	printf("]\n");
 }
 
-/* 힙을 트리모양으로 출력하는 함수*/
-void PrintHeapTree(Heap * heap, char* szTitle) {
-	printf("================================\n%s\n================================\n ", szTitle);
-	int i = 1, sizeHeap = (heap->numOfData);
-
-	while (i <= sizeHeap) {
-		if (i * 2 > sizeHeap)
-			break;
-		i = i * 2;
+int TabCount(int height) {
+	int cnt = 1;
+	if (height) {
+		for (int i = 1; i <= height; i++) cnt *= 2;
 	}
-
-	int front = i - 1;
-	int between = i * 2 + 1;
-	int count = 1;
-	int index = 1;
-	printf("%d\n", i);
-
-	while (count <= sizeHeap) {
-		int ftemp = front;
-		while (ftemp--)
-			printf("\t");
-
-		int ctemp = count;
-		while (ctemp--) {
-			if (index > sizeHeap) {
-				printf("\n");
-				return;
-			}
-			printf("%d", heap->heapArr[index++]);
-			if (index != 2) {
-				int btemp = between;
-				while (btemp--)
-					printf("\t");
-			}
-		}
-		count *= 2;
-		front /= 2;
-		between /= 2;
-		printf("\n\n\n");
-	}
-	printf("\n");
+	return cnt - 1;
 }
 
-int main(void) {
+/* 힙을 트리모양으로 출력하는 함수*/
+void PrintHeapTree(Heap * heap, char* szTitle) {
+	printf("================================\n%s\n================================\n", szTitle);
+	int height = 1, sizeHeap = (heap->numOfData);
+	int cnt = 0, idx = 1, child = 1, temp;
+
+	while (height <= sizeHeap) {
+		height = height * 2;
+		if (height > sizeHeap) break;
+		cnt++;
+	}
+
+	temp = cnt;
+
+	for (cnt; cnt >= 0; cnt--) {
+		printf("[LEVEL : %d]\t", temp - cnt);
+		for (int t = 0; t < child; t++) {
+			for (int tap = 0; tap < TabCount(cnt); tap++)
+				printf(". ");
+			if (idx >= sizeHeap) break;
+			printf("%2d", heap->heapArr[idx++]);
+			for (int tap = 0; tap < TabCount(cnt) + 1; tap++) {
+				printf(". ");
+			}
+		}
+		printf("\n");
+		child *= 2;
+	}
+}
+
+int main(void)
+{
 	Heap heap;
 	int i, nSizeItemList = 0;
 
-	int ItemList[] = { 50, 40, 30, 25, 30, 20, 10, 5, 15, 20 };
+	//int ItemList[] = { 24, 17, 29, 22, 20, 31, 27, 18, 21 };	// 수행평가 10번
+	//int ItemList[] = { 4, 16, 57, 9, 32, 45, 20, 19 };		// 수행평가 11번
+	int ItemList[] = { 50, 40, 30, 25, 30, 20, 10, 5, 15, 20, 12, 15, 16, 14, 16, 45, 75, 86, 34, 76, 34, 34, 76, 34, 23, 67, 98, 34, 34, 76, 34, 23, 67, 98, 34, 34, 76, 34, 23, 67, 98, 34, 34, 76, 34, 23, 67, 98, 34, 34, 76, 23, 67, 98 };	// 수행평가 12번
+	//int ItemList[] = { 50, 40, 30, 25, 30 };
 
 	nSizeItemList = sizeof(ItemList) / sizeof(int);
 
 	HeapInit(&heap, DataPriorityComp);
 
-	for (i = 0; i < nSizeItemList; i++) {
+	for (i = 0; i < nSizeItemList; i++)
+	{
 		HInsert(&heap, ItemList[i]);
 	}
 

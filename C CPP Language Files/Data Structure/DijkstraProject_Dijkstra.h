f29d484cb28442c6c90ca861aae006e2847d4cde
gimhaesanai <jinjin0816@naver.com>
2019-11-11 21:10:26 +0900

    DijkstraProject_Dijkstra

---
+++ C CPP Language Files/Data Structure/DijkstraProject_Dijkstra.h
@@ -0,0 +1,5 @@
+#define INF 100000
+#define VER_NUM 5
+
+typedef int DData;
+
---
+++ C CPP Language Files/Data Structure/DijkstraProject_DijkstraMain.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "Dijkstra.h"
+
+int main() {
+	DData Weight[VER_NUM + 1][VER_NUM + 1];
+
+	return 0;
+}

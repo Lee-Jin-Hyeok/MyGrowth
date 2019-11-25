7ae65937ed6f3e017fa46785cd67bf27da0bd4df
gimhaesanai <jinjin0816@naver.com>
2019-11-25 23:30:14 +0900

    userdao

--- Project Temp/Project_DDaMamIn/DDaMamIn/WebContent/join2.jsp
+++ Project Temp/Project_DDaMamIn/DDaMamIn/WebContent/join2.jsp
@@ -1,10 +1,14 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="EUC-KR"%><%!
+    pageEncoding="EUC-KR"
+    import="user.*, java.util.*" %><%!
     String ctxPath = null;
+    UserDAO udao = null;
+    List<UserVO> uls = null;
 %><%
 	ctxPath = request.getContextPath();
 	request.setCharacterEncoding("UTF-8");
 	
-
-	response.sendRedirect( ctxPath + "/index.jsp" );
+	udao = new UserDAO();
+	
+	response.sendRedirect( ctxPath + "/main.jsp" );
 %><!DOCTYPE html>
--- Project Temp/Project_DDaMamIn/DDaMamIn/src/user/UserDAO.java
+++ Project Temp/Project_DDaMamIn/DDaMamIn/src/user/UserDAO.java
@@ -1,5 +1,32 @@
 package user;
 
-public class UserDAO {
+import java.util.List;
 
+import jdbcUtil.JdbcTemplate;
+import rowmapper.UserRowMapper;
+
+public class UserDAO {
+	JdbcTemplate jdbc = new JdbcTemplate();
+	
+	public void insert(UserVO vo) {
+		String sql = "insert into user"
+				+ "(u_pw, u_name, u_email) "
+				+ "values(?,?,?)";
+		try {
+			jdbc.update(sql, vo.getU_pw(), vo.getU_name(), vo.getU_email());
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public List<UserVO> findAll() {
+		List<UserVO> ls = null;
+		String sql = "select  * from user";
+		try {
+			ls = jdbc.query(sql, new UserRowMapper());
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		return ls;
+	}
 }

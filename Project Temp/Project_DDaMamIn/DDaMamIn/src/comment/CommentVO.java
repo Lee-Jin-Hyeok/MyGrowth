46ee943cb9596578f7e47733ae75e0ff47bf7e45
gimhaesanai <jinjin0816@naver.com>
2019-11-25 22:25:34 +0900

    database comment vo

---
+++ Project Temp/Project_DDaMamIn/DDaMamIn/src/comment/CommentVO.java
@@ -0,0 +1,40 @@
+package comment;
+
+public class CommentVO {
+	private Integer c_id = null;
+	private String c_content = null;
+	private String c_date = null;
+	private String u_id = null;
+	private Integer p_id = null;
+	
+	public Integer getC_id() {
+		return c_id;
+	}
+	public void setC_id(Integer c_id) {
+		this.c_id = c_id;
+	}
+	public String getC_content() {
+		return c_content;
+	}
+	public void setC_content(String c_content) {
+		this.c_content = c_content;
+	}
+	public String getC_date() {
+		return c_date;
+	}
+	public void setC_date(String c_date) {
+		this.c_date = c_date;
+	}
+	public String getU_id() {
+		return u_id;
+	}
+	public void setU_id(String u_id) {
+		this.u_id = u_id;
+	}
+	public Integer getP_id() {
+		return p_id;
+	}
+	public void setP_id(Integer p_id) {
+		this.p_id = p_id;
+	}
+}

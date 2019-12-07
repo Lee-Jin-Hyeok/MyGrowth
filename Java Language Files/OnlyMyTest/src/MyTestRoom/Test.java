5d6af1c2e6eb95bdb104dc899f27eda4952781e5
gimhaesanai <jinjin0816@naver.com>
2019-12-07 23:24:57 +0900

    kotlin

--- Java Language Files/OnlyMyTest/src/MyTestRoom/Test.java
+++ Java Language Files/OnlyMyTest/src/MyTestRoom/Test.java
@@ -1,15 +1,29 @@
 package MyTestRoom;
 
+//public class Test {
+//	public static void main(String[] args) {
+//		Test t = new Test();
+//		System.out.println(t.isString("Hello"));
+//		System.out.println(t.isString(10));
+//	}
+//	public int isString(Object obj) {
+//		if(obj instanceof String)
+//			return ((String) obj).length();
+//		else
+//			return (Integer) null;
+//	}
+//}
+
+
+class GenericTest<T> {
+	public T add(T a, T b) {
+		return (T) (a + " " + b);
+	}
+}
+
 public class Test {
 	public static void main(String[] args) {
-		Test t = new Test();
-		System.out.println(t.isString("Hello"));
-		System.out.println(t.isString(10));
-	}
-	public int isString(Object obj) {
-		if(obj instanceof String)
-			return ((String) obj).length();
-		else
-			return (Integer) null;
+		GenericTest<String> t = new GenericTest<String>();
+		System.out.println(t.add("Hello", "World"));
 	}
 }

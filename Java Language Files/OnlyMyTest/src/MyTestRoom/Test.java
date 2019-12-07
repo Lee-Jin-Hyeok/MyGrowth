7c88ed8b632707cdd7d4579654c4c4ce60fd68f8
gimhaesanai <jinjin0816@naver.com>
2019-12-07 11:15:24 +0900

    kotlin study

--- Java Language Files/OnlyMyTest/src/MyTestRoom/Test.java
+++ Java Language Files/OnlyMyTest/src/MyTestRoom/Test.java
@@ -1,41 +1,15 @@
 package MyTestRoom;
 
-interface Person {
-	String getName();
-	int getAge();
-	void ShowData();
-}
-
-class LeeJinHyeok implements Person {
-	private String name;
-	private int age;
-	
-	public LeeJinHyeok(String name, int age) {
-		this.name = name;
-		this.age = age;
-	}
-	
-	@Override
-	public String getName() {
-		return this.name;
-	}
-	@Override
-	public int getAge() {
-		return this.age;
-	}
-	@Override
-	public void ShowData() {
-		System.out.println("[ Á¤º¸ ]");
-		System.out.println("Name : " + name);
-		System.out.println("Age : " + age);
-	}
-}
-
 public class Test {
 	public static void main(String[] args) {
-		Person person;
-		person = new LeeJinHyeok("lee jin hyeok", 17);
-		
-		person.ShowData();
+		Test t = new Test();
+		System.out.println(t.isString("Hello"));
+		System.out.println(t.isString(10));
+	}
+	public int isString(Object obj) {
+		if(obj instanceof String)
+			return ((String) obj).length();
+		else
+			return (Integer) null;
 	}
 }

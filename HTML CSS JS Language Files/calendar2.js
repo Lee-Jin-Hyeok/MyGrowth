b94e691d4702fc960ed4769861d1678e1c06e901
gimhaesanai <jinjin0816@naver.com>
2019-12-27 23:26:37 +0900

    calendar

--- HTML CSS JS Language Files/calendar2.js
+++ HTML CSS JS Language Files/calendar2.js
@@ -24,24 +24,59 @@ function ThisMonthFirstDay(year, month) {
     var arr = [31, 28+yun, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
     for(var i = 0 ; i < month-1 ; i++)
         day += arr[i];
-
+    console.log("FirstDay : " + day%7);
     return day % 7;
 }
 
 function LastDay(year, month) {
+    if(month < 1) {
+        year-=1;
+        month = 12;
+    }
     var yun = 0;
     if(((year%4 == 0 ) && (year%100 != 0)) || (year%400 == 0))
         yun = 1;
     var arr = [31, 28+yun, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    console.log("LastDay : " + arr[month-1]);
     return arr[month-1];
 }
 
+function Day(month, day, color, special) {
+    this.month = month;
+    this.day = day;
+    this.color = color;
+    this.special = special;
+}
+
 function CreateAMonthlyCalendar(year, month) {
-    var arr = [[],[],[],[],[],[]];
-    console.log("FirstDay : " + ThisMonthFirstDay(year, month));
+    var calendar = [[],[],[],[],[],[]];
 
     var firstDay = ThisMonthFirstDay(year, month);
     var lastDay = LastDay(year, month);
+    var beforeLastDay = LastDay(year, month-1) - firstDay + 1;
+    var date = 0;
+    var nextDate = 0;
+
+    for(var i = 0 ; i < 6 ; i++) {
+        for(var j = 0 ; j < 7 ; j++) {
+            if(i == 0 && j < firstDay)
+                calendar[i][j] = new Day(month-1, beforeLastDay+j, "GREY", "");
+            else if(date >= lastDay)
+                calendar[i][j] = new Day(month+1, ++nextDate, "GREY", "");
+            else {
+                if(j == 0)
+                    calendar[i][j] = new Day(month, ++date, "RED", "");
+                else if(j == 6)
+                    calendar[i][j] = new Day(month, ++date, "BLUE", "");
+                else
+                    calendar[i][j] = new Day(month, ++date, "BLACK", "");
+            }
+        }
+    }
+
+    return calendar;
 }
 
-CreateAMonthlyCalendar(2019, 3);
+var calendar = CreateAMonthlyCalendar(2019, 12);
+
+console.log(calendar);
